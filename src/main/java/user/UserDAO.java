8c9b2da754e05a15a3ab89a08dae969305613762
devopskickoff <devopskick@gmail.com>
2021-08-19 22:12:11 +0900

    Feat board update , delete

--- src/main/java/user/UserDAO.java
+++ src/main/java/user/UserDAO.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
-public class UserDAO {
+public class UserDAO {  
 	
 	private Connection conn;   
 	private PreparedStatement pstmt;
